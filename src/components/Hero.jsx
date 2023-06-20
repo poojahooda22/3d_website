091258317621e2c11ea419da5549d8f0f1f690e6
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-06-20 23:14:45 +0530

    third

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import {motion} from 'framer-motion';
+// import {motion} from 'framer-motion';
 
 import {styles} from '../styles';
 import {ComputersCanvas} from './canvas';
@@ -9,10 +9,21 @@ const Hero = () => {
             <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
                 <div className='flex flex-col justify-center items-center mt-5'>
                     <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-                    <div/>
+                    <div className='w-1 sm:h-80 h-40 violent-gradient'/>
                 </div>
-                
+
+                <div>
+                    <h1 className={`${styles.heroHeadText} 
+                    text-white`}>Hi, I am<span
+                    className='text-[#915eff]'>Pooja</span></h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                        I develop 3D visuals, user<br className='sm:block hidden'/>interfaces
+                        and web applications
+                    </p>
+                </div>  
             </div>
+
+            <ComputersCanvas/>
 
         </section>
     )
