6189760eaaee8925e5037e32ec0c96ae4a4a647b
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-06-28 22:35:39 +0530

    first

--- src/components/convas/Computers.jsx
+++ src/components/convas/Computers.jsx
@@ -15,7 +15,8 @@ const Computers = () => {
             <spotLight
                 position={[-20, 50, 10]}
                 angle={0.12}
-                penumbra={1}/>
+                penumbra={1}
+                intensity={1}/>
             <primitive
             object={computer.scene}
             scale={0.75}
