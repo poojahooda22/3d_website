142f4d6969082caa16cd14b81f27bd4bb27e6949
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-06-29 23:06:43 +0530

    fourth

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import {Html, useProgress} from '@react-three/drei';
 
 const Loader =() => {
+    const{progress} = useProgress();
+
     return (
         <div></div>
     )
