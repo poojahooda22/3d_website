b69b48422fff9416784a05a94e39c1246c86dd2f
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-07-17 23:00:40 +0530

    first

--- src/components/convas/Computers.jsx
+++ src/components/convas/Computers.jsx
@@ -59,7 +59,7 @@ const ComputersCanvas = () => {
                 maxPolarAngale={Math.PI / 2}
                 minPolarAngle={Math.PI / 2}
                 />
-                <Computers/>
+                <Computers isMobile/>
             </Suspense>
 
             <Preload all/>
