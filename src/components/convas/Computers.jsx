6e75b42a7dd9c140d27e474e71cddf1df0f01337
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-07-18 22:55:07 +0530

    third

--- src/components/convas/Computers.jsx
+++ src/components/convas/Computers.jsx
@@ -11,14 +11,14 @@ const Computers = ({ isMobile }) => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
-      {/* <spotLight
+      <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
-      /> */}
+      />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
