604796d511294d1b29bea627774b5b235d5d8daa
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-07-19 23:19:24 +0530

    second

--- src/components/convas/Computers.jsx
+++ src/components/convas/Computers.jsx
@@ -19,7 +19,7 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      {/* <pointLight intensity={1} /> */}
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
