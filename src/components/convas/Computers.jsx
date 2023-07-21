dbf05ae27dfee3c72778f136a17065fbd4cd3a49
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-07-21 23:10:11 +0530

    third

--- src/components/convas/Computers.jsx
+++ src/components/convas/Computers.jsx
@@ -50,7 +50,7 @@ const ComputersCanvas = () => {
 
     // Remove the listener when the component is unmounted
     return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+      // mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
 
