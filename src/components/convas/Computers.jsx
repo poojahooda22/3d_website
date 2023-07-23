0db3777be3816087a5381532dd872819139318d8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-07-23 22:48:44 +0530

    second

--- src/components/convas/Computers.jsx
+++ src/components/convas/Computers.jsx
@@ -35,7 +35,7 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    // const mediaQuery = window.matchMedia("(max-width: 500px)");
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
